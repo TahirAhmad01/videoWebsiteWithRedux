1a1ec71db847fc17ddfed71716bd3e5bd49beea4
Tahir Ahmad <tahirahmad0234@gmail.com>
2022-10-01 18:29:03 +0600

    video website add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 // import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Layout } from "./components/layout";
+import Home from "./pages/Home";
 import Video from "./pages/Video";
 
 function App() {
-    return <Video />;
+  return (
+    <>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="videos/:videoId" element={<Video />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </>
+  );
 }
 
 export default App;
